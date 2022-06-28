fbe95ba6d95f9406f720ac6869160e409d260c9d
holferfy <holger.fy@outlook.com>
2022-06-28 14:25:04 +0800

    update load config

--- config/config.go
+++ config/config.go
@@ -2,13 +2,10 @@ package config
 
 import (
 	"errors"
-	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/holgerfy/go-pkg/log"
 	jsoniter "github.com/json-iterator/go"
 	"io/ioutil"
-	"os"
-	"path/filepath"
 	"strings"
 	"sync"
 )
@@ -41,27 +38,6 @@ func (conf *Config) copy(node string, value map[string]interface{}) {
 	}
 }
 
-func (conf *Config) walk(path string, info os.FileInfo, err error) error {
-	if err == nil {
-		if !info.IsDir() {
-			if !strings.HasSuffix(path, ".toml") {
-				return nil
-			}
-			var err error
-			var config map[string]interface{}
-			_, err = toml.DecodeFile(path, &config)
-			if err != nil {
-				fmt.Println("aaf: ", path, err)
-				log.Logger().Fatal(nil, err)
-			}
-			conf.copy(strings.TrimSuffix(info.Name(), ".toml"), config)
-		} else {
-			return filepath.Walk(info.Name(), conf.walk)
-		}
-	}
-	return nil
-}
-
 func (conf *Config) load(path []string) *Config {
 	conf.once.Do(func() {
 		conf.configs = make(map[string]map[string]interface{})
@@ -71,7 +47,15 @@ func (conf *Config) load(path []string) *Config {
 				continue
 			}
 			for _, fi := range rd {
-				conf.walk(dir+"/"+fi.Name(), fi, nil)
+				if !fi.IsDir() {
+					if strings.HasSuffix(dir+fi.Name(), ".toml") {
+						var config map[string]interface{}
+						if _, err = toml.DecodeFile(dir+fi.Name(), &config); err != nil {
+							log.Logger().Fatal(nil, "failed to load config")
+						}
+						conf.copy(strings.TrimSuffix(fi.Name(), ".toml"), config)
+					}
+				}
 			}
 		}
 	})
